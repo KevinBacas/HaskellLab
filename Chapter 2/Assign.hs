1b2b09636b4ae00e4705d061a323916fa97f3db1
kevinbacas <bacas.kevin@hotmail.fr>
2015-09-10 23:02:51 +0200

    Added some explanations

--- Chapter 2/Assign.hs
+++ Chapter 2/Assign.hs
@@ -1,3 +1,5 @@
 -- file: ch02/Assign.hs
+-- Explanation : This file will not compile because we cannot declare the same
+-- variable twice.
 x = 10
 x = 11
--- Chapter 2/myDrop.hs
+++ Chapter 2/myDrop.hs
@@ -1,2 +1,9 @@
 -- file: ch02/myDrop.hs
+-- Explanation : Single line implementation for drop
+-- Python equivalent :
+-- def myDrop(n, elts):
+--     while n > 0 and elts:
+--         n = n - 1
+--         elts = elts[1:]
+--     return elts
 myDropX n xs = if n <= 0 || null xs then xs else myDropX (n - 1) (tail xs)
